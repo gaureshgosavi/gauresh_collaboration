370473599f6ba76562e7248535db790a7e23b152
Gauresh Gosavi <gaureshgosavi10@gmail.com>
2017-01-10 09:34:24 +0530

    worked on UI. now working on cruds of user module.

--- collaboration_backend/collaboration_backend/src/main/java/com/niit/collaboration_backend/controller/JobApplied.java
+++ collaboration_backend/collaboration_backend/src/main/java/com/niit/collaboration_backend/controller/JobApplied.java
@@ -1,5 +1,9 @@
 package com.niit.collaboration_backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JobApplied {
 
+	
 }
---
+++ online_collaboration/app/components/user/userFactory.js
@@ -0,0 +1,52 @@
+app.factory('userFactory', ['$http', '$q', function ($http, $q) {
+    var url = 'http://localhost:8090/collaboration_backend/user/';
+
+    return {
+        getAllUsers: getAllUsers,
+        createUser: createUser,
+        updateUser: updateUser,
+    };
+
+    function getAllUsers() {
+        debugger;
+        var deferred = $q.defer();
+        $http.get(url + 'list').
+            then(function (response) {
+                deferred.resolve(response.data);
+            }, function (errResponse) {
+                deferred.reject(errResponse);
+            });
+        return deferred.promise;
+    }
+
+       function createUser(user) {
+        var deferred = $q.defer();
+        $http.post(url+'register', user)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while creating User');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+ 
+ 
+    function updateUser(user, id) {
+        var deferred = $q.defer();
+        $http.put(url+'user/get'+id, user)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while updating User');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+}])
